ceea73011d9a0c82efba095bc1e94ec8a94c60cc
WinnieOmollo46 <winnieomollo46@gmail.com>
2022-09-20 19:27:52 -0700

    4-print_rev.c

---
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_rev - print a string in reverse
+ * @s: argument to the function
+ * Return: return void
+ */
+
+void print_rev(char *s)
+{
+	int length;
+
+	for (length = 0; s[length] != '\0'; length++)
+	{
+	}
+	for (length = length - 1; length >= 0; length--)
+	{
+		_putchar(s[length]);
+	}
+	_putchar('\n');
+}
