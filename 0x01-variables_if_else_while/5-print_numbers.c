37acfceafb5beb80b77e3e3a4058d26a006a4bc7
WinnieOmollo46 <winnieomollo46@gmail.com>
2022-09-22 01:52:16 -0700

    5-print_numbers.c

--- 0x01-variables_if_else_while/5-print_numbers.c
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -2,13 +2,13 @@
 
 /**
  * main - Entry point
- *  Return: Always (0) success
-*/
+ * Return: Always (0) success
+ **/
 
 int main(void)
 {
 	int num;
-	
+
 	for (num = 0; num < 10; num++)
 	{
 		printf("%d", num);
